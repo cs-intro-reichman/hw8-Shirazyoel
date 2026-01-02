bda8a5a135dcf4ed00e580367673ba973bfb2d38
Shiraz Yoel <shiraz.jaul@post.runi.ac.il>
2026-01-02 20:06:52 +0200

    d

--- Network.java
+++ Network.java
@@ -113,7 +113,7 @@ public class Network {
             String currentName = users[i].getName();
             int currentCount = followeeCount(currentName);
             
-            if (currentCount >= maxFollowers){
+            if (currentCount > maxFollowers){
                  Most = currentName;
                 Most = currentName;
             }
@@ -141,7 +141,7 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
        //// Replace the following statement with your code
-       String ans = "Network: ";
+       String ans = "Network:";
 
     for (int i = 0; i < userCount; i++) {
         ans += users[i].toString();
